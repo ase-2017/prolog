c0203826ac9de3c611799dbba4380fd2d8b8be01
dummy <dummy@github.com>
2020-03-28 19:49:27 +0100

    Added a little more help text

--- prolog01/e01.pl
+++ prolog01/e01.pl
@@ -16,20 +16,24 @@ child(maria,claudia).
 
 % clauses look like this
 % parent(Child,Parent):- child(Parent,Child).
-% Arguments/Variables start with a capital letter
+% Arguments/Variables must start with a capital letter
 % often variable names have all capital letters like shown below
 parent(CHILD,PARENT):- child(PARENT,CHILD).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Select Prolog perspective -> Window->Perspective->Other->Prolog
+% If this file was just opened, press F9 to let Prolog process it
+% Saving after changing it lets Prolog automatically reprocess it with your changes. 
 
-% You can run a query by entering
+
+% You can run a query in the Prolog Console by entering
 % the string "e01:parent(claudia,PARENT)." in the prolog console
-% pressing space will produce more results if available or 'false' if no more results are found
+% pressing space will produce more results if available until you see the prompt "<num> ?-" again
+% If you see 'false' no results was found for your clause.
   
   
 % Your task: create a set of clauses (and facts) so that you can find out the name of your grandparents
 % with the query "e01:grandparent(kevin,Grandparent)."
-
-
 
 
 	
